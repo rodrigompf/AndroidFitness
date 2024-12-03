e3c26db8548f593638edffa2f3d8ca7eee2adf38
vanori <alexvaarino7@gmail.com>
2024-12-03 20:14:16 +0000

    sigma

--- app/src/main/java/_usersProfiles/Profile.kt
+++ app/src/main/java/_usersProfiles/Profile.kt
@@ -1,4 +1,4 @@
-package com.example.androidfitness
+package _usersProfiles
 
 import android.content.Intent
 import android.os.Bundle
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.androidfitness.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
--- app/src/main/java/_usersProfiles/UserProfiles.kt
+++ app/src/main/java/_usersProfiles/UserProfiles.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.androidfitness.Profile
 import com.example.androidfitness.R
 
 class UserProfiles : AppCompatActivity() {
